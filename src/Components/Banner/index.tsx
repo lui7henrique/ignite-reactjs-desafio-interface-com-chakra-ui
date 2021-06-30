b754ba5c320a209914c7e813738c6f97a2743d02
Luiz Henrique <7henrique18@gmail.com>
2021-06-30 19:39:18 -0300

    :gear: Fix to deploy

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -38,7 +38,7 @@ export function Banner() {
         <Image
           w={["300px", "300px", "300px", "430px"]}
           display={["none", "none", "block"]}
-          src="/assets/airplane.svg"
+          src="/assets/Airplane.svg"
           alt="Avião"
           transform="translateY(48px)"
           ml="8px"
